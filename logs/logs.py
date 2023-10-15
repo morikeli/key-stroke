f53bff7b49698a0fed8e16c7ae6d8e0de1ef354e
brocode <kelimbth@gmail.com>
2023-10-15 16:44:29 +0300

    updated save_logs() the functions writes date when the file was created. import TextWrapper class to wrap typed words in a text file

--- logs/logs.py
+++ logs/logs.py
@@ -1,3 +1,17 @@
+from textwrap import TextWrapper
+from datetime import datetime
+
 def save_logs(key_input):
-    with open('logs.txt', 'a') as f:
-        f.write(key_input)
+    """ This function writes and saves key strokes in a text file. """
+    
+    text_wrap = TextWrapper(width=10)
+    
+    with open('logs.txt', 'a+') as logfile:
+        with open('logs.txt', 'r') as f:
+            if str(datetime.now().strftime("%a %d-%m-%Y")) not in f.read():
+                print('==='*30, file=logfile)
+                print(f'New log file - {datetime.now().strftime("%a %d-%m-%Y, %H:%M:%S")}', file=logfile)
+                print('==='*30, file=logfile)
+                print('\n', file=logfile)
+
+        logfile.write(text_wrap.fill(key_input))
